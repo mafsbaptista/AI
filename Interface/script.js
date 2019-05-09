ae501956ac2a9a902bc22f1ec9da6e900e57b169
mafsbaptista <mafa.ferreira96@gmail.com>
2019-05-09 21:32:53 +0100

    #9 Add slider initialization.

--- Interface/script.js
+++ Interface/script.js
@@ -2,17 +2,23 @@ const server_url = "https://homecontrolserver.herokuapp.com";
 
 function init() {
   const inputs = $('div').find('input');
+  let fields;
   for(let i in inputs){
     if(inputs[i].type === 'checkbox') {
-      let fields = getFields(inputs[i].name);
+      fields = getFields(inputs[i].name);
       if (fields.length == 3) {
         getStateCheckBox(inputs[i], fields[0], fields[1], fields[2]);
       } else {
         //Get Server fields[0], fields[1]
       }
+    } else if (inputs[i].type === 'range') {
+      let fields = getFields(inputs[i].name);
+      getStateRange(inputs[i], fields[0], fields[1], fields[2]);
+
     }
   }
 }
+
 
 function getStateCheckBox(checkbox, division, type, object) {
   const request_url = server_url + "/" + division + "/" + type + "/" + object;
@@ -36,7 +42,34 @@ function getStateCheckBox(checkbox, division, type, object) {
     });
 }
 
-function changeState(checkbox, division, type, object) {
+function getStateRange(range, division, type, object) {
+  let output = document.getElementById(range.id + "_range");
+  console.log(output);
+  const request_url = server_url + "/" + division + "/" + type + "/" + object;
+  const headers = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  };
+
+  const params = {
+    method: "GET",
+    mode: "cors",
+  };
+
+  fetch(request_url, {
+    headers,
+    params
+  })
+  .then(response => response.json())
+  .then(response => {
+    console.log(range);
+    console.log(response);
+    range.value = response;
+    output.innerHTML = response;
+  });
+}
+
+function changeCheckBoxState(checkbox, division, type, object) {
   const request_url = server_url + "/" + division + "/" + type + "/" + object;
 
   const newstate = (checkbox.checked ? "1" : "0");
