95c6cbb13f095348937ed8dfc6868e85c57f8d58
mafsbaptista <mafa.ferreira96@gmail.com>
2019-05-09 22:41:36 +0100

    #9 Add all checkox requests.

--- Interface/script.js
+++ Interface/script.js
@@ -6,10 +6,10 @@ function init() {
   for(let i in inputs){
     if(inputs[i].type === 'checkbox') {
       fields = getFields(inputs[i].name);
-      if (fields.length == 3) {
+      if (fields.length === 3) {
         getStateCheckBox(inputs[i], fields[0], fields[1], fields[2]);
       } else {
-        //Get Server fields[0], fields[1]
+        getStateAllEqual(inputs[i], fields[0], fields[1]);
       }
     } else if (inputs[i].type === 'range') {
       let fields = getFields(inputs[i].name);
@@ -17,6 +17,28 @@ function init() {
 
     }
   }
+}
+
+function getStateAllEqual(checkbox, division, type) {
+  const request_url = server_url + "/" + division + "/" + type;
+  const headers = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  };
+
+  const params = {
+    method: "GET",
+    mode: "cors",
+  };
+
+  fetch(request_url, {
+    headers,
+    params
+  })
+  .then(response => response.json())
+  .then(response => {
+      checkbox.checked = response >= 1;
+  });
 }
 
 
@@ -83,7 +105,7 @@ function changeCheckBoxState(checkbox, division, type, object) {
   });
 
   fetch(request)
-  .then(function() {});
+  .then(init());
 }
 
 function changeRangeState(range, division, type, object) {
@@ -105,6 +127,26 @@ function changeRangeState(range, division, type, object) {
 
   fetch(request)
   .then(output.innerHTML = newstate);
+}
+
+function requestStateAllEqual(checkbox, division, type) {
+  const request_url = server_url + "/" + division + "/" + type;
+
+  const newstate = (checkbox.checked ? "1" : "0");
+  const state = {state: newstate };
+
+  const request = new Request(request_url, {
+    method: 'POST',
+    mode: 'cors',
+    redirect: 'follow',
+    headers: new Headers({
+      'Content-Type': 'application/json',
+    }),
+    body: JSON.stringify(state),
+  });
+
+  fetch(request)
+  .then(init());
 }
 
 function getFields(string) {
